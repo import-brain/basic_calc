1e88a1468a64774b6533f35bb6b9f95f0aa3191a
ericcheng2005 <68196762+ericcheng2005@users.noreply.github.com>
2021-05-07 22:44:39 -0400

    Update calculator.py
    added square root using math module

--- calculator.py
+++ calculator.py
@@ -1,3 +1,6 @@
+import math
+
+
 # adding
 
 def add(x,y):
@@ -28,6 +31,11 @@ def exponent(x,y):
 def modulo(x,y):
     return x % y
 
+def sqrroot(x):
+    return math.sqrt(x)
+
+
+
 # User prompt to select operation
 
 print("Select operation")
@@ -37,12 +45,17 @@ print("3. multiply")
 print("4. divide")
 print("5. exponent")
 print("6. modulo/remainder")
+print("7. square root")
 
 # Making the calculator work
 
 while True:
 
-    choice = input("Enter choice(1/2/3/4/5/6): ")
+    choice = input("Enter choice(1/2/3/4/5/6/7): ")
+
+    if choice in ('7'):
+        number_sqr = float(input("Enter number:"))
+        print("The square root of", number_sqr, "is", sqrroot(number_sqr))
 
     if choice in ('1', '2', '3', '4', '5', '6'):
         number_1 = float(input("Enter first number: "))
@@ -66,8 +79,8 @@ while True:
         elif choice == '6':
             print("The remainder/modulo of", number_1, "and", number_2, "is", modulo(number_1,number_2))
         break
-    else:
-        print("Invalid input: select operation")
+else:
+    print("Invalid input: select operation")
 
 # :)
 
