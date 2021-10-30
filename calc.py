1ddefd76edb8b14a94f8be7a14f211d3ce660c42
HenrySoftwareStudio <86927130+HenrySoftwareStudio@users.noreply.github.com>
2021-10-30 11:45:16 -0400

    Add files via upload

--- calc.py
+++ calc.py
@@ -1,224 +1,13 @@
 import math
-
 import sys
-
 import time
 
-
-# adding
-
-def calculator():
-    def add(x, y):
-        return x + y
-
-    # subtracting
-
-    def subtract(x, y):
-        return x - y
-
-    # multiplying
-
-    def multiply(x, y):
-        return x * y
-
-    # dividing
-
-    def divide(x, y):
-        return x / y
-
-    # exponentiation
-
-    def exponent(x, y):
-        return x ** y
-
-    # modulo/remainder/whatever you want to call it
-
-    def modulo(x, y):
-        return x % y
-
-    # square root
-
-    def sqrroot(x):
-        return math.sqrt(x)
-
-    # circumference
-
-    def circumference(x):
-        return pi * x
-
-    # area of circle
-
-    def circle_area(x):
-        return pi * (x ** 2)
-
-    # sin, cos, tan
-
-    def sin(x):
-        return math.sin(x)
-
-    def cos(x):
-        return math.cos(x)
-
-    def tan(x):
-        return math.tan(x)
-
-    def rad(x):
-        return math.radians(x)
-
-    def deg(x):
-        return math.degrees(x)
-
-    def rad_or_degree(): # rad or degree choice picker function
-        rad_or_degree = input("Degrees or radians? Enter 0 for degrees, 1 for radians: ")
-
-        if rad_or_degree != '1' and rad_or_degree != '0':
-            print("Must select 1 or 0")
-            time.sleep(2)
-            rad_or_degree()
-        elif rad_or_degree == '1':
-            return 1
-        elif rad_or_degree == '0':
-            return 0
-
-    pi = 3.14
-
-    # User prompt to select operation
-
-    print("Select operation")
-    print("1. add")
-    print("2. subtract")
-    print("3. multiply")
-    print("4. divide")
-    print("5. exponent")
-    print("6. modulo/remainder")
-    print("7. square root")
-    print("8. circumference")
-    print("9. area of circle")
-    print("10. sine")
-    print("11. cosine")
-    print("12. tangent")
-    print("13. convert degrees to radians")
-    print("14. convert radians to degrees")
-    print("15. read previous calculations")
-
-    # Making the calculator work
-
-    while True:
-        # todo: fix spacing issues in output to file
-        # For every choice, execute operations based on user input
-
-        choice = input("Enter choice(1/2/3/4/5/6/7/8/9/10/11/12/13/14/15): ")
-
-        if choice in ('1', '2', '3', '4', '5', '6'):
-            number_1 = float(input("Enter first number: "))
-            number_2 = float(input("Enter second number: "))
-
-            if choice == '1':
-                print(number_1, "+", number_2, "=", add(number_1, number_2))
-                exit_flow(str(number_1) + "+" + str(number_2) + "=" + str(add(number_1, number_2)))
-
-            elif choice == '2':
-                print(number_1, "-", number_2, "=", subtract(number_1, number_2))
-                exit_flow(str(number_1) + "-" + str(number_2) + "=" + str(subtract(number_1, number_2)))
-
-            elif choice == '3':
-                print(number_1, "*", number_2, "=", multiply(number_1, number_2))
-                exit_flow(str(number_1) + "*" + str(number_2) + "=" + str(multiply(number_1, number_2)))
-
-            elif choice == '4':
-                try: # if user attempts to divide by 0, return error message
-                    x = divide(number_1, number_2)
-                except ZeroDivisionError:
-                    print("Oops! Numbers cannot be divided by 0")
-                else:
-                    print(number_1, "/", number_2, "=", divide(number_1, number_2))
-                    exit_flow(str(number_1) + "/" + str(number_2) + "=" + str(divide(number_1, number_2)))
-
-            elif choice == '5':
-                print(number_1, "to the power of", number_2, "=", exponent(number_1, number_2))
-                exit_flow(str(number_1) + "to the power of" + str(number_2) + "=" + str(exponent(number_1, number_2)))
-            elif choice == '6':
-                try: # if user attempts to divide by 0, return error message
-                    x = modulo(number_1, number_2)
-                except ZeroDivisionError:
-                    print("Oops! Numbers cannot be divided by 0")
-                else:
-                    print("The remainder/modulo of", number_1, "and", number_2, "is", modulo(number_1, number_2))
-                    exit_flow("The remainder/modulo of" + str(number_1) + "and" + str(number_2) + "is" + str(modulo(number_1, number_2)))
-        
-        if choice == '7':
-            number_sqr = float(input("Enter number:"))
-            print("The square root of", number_sqr, "is", sqrroot(number_sqr))
-            exit_flow("The square root of" + str(number_sqr) + "is" + str(sqrroot(number_sqr)))
-
-        elif choice == '8':
-            circle_diameter = float(input("Diameter of circle:"))
-            print("A circle with diameter", circle_diameter, "has a circumference of", circumference(circle_diameter))
-            exit_flow("A circle with diameter" + str(circle_diameter) + "has a circumference of" + str(circumference(circle_diameter)))
-
-        elif choice == '9':
-            circle_radius = float(input("Radius of circle:"))
-            print("A circle with radius", circle_radius, "has an area of", circle_area(circle_radius))
-            exit_flow("A circle with radius" + str(circle_radius) + "has an area of" + str(circle_area(circle_radius)))
-
-        elif choice == '10':
-            type_choice = rad_or_degree()
-            x = float(input("Number to be sined: "))
-            if type_choice == 0:
-                print("The sine of ", x, " degrees is ", round(sin(math.radians(x)), 3))
-                exit_flow("The sine of " + str(x) + " degrees is " + str(round(sin(math.radians(x)), 3)))
-            elif type_choice == 1:
-                print("The sine of ", x, " radians is ", round(sin(x), 3))
-                exit_flow("The sine of " + str(x) + " radians is " + str(round(sin(x), 3))) 
-
-        elif choice == '11':
-            type_choice = rad_or_degree()
-            x = float(input("Number to be cosined: "))
-            if type_choice == 0:
-                print("The cosine of ", x, " degrees is ", round(cos(math.radians(x)), 3))
-                exit_flow("The cosine of " + str(x) + " degrees is " + str(round(cos(math.radians(x)), 3)))
-            elif type_choice == 1:
-                print("The cosine of ", x, " radians is ", round(cos(x), 3))
-                exit_flow("The cosine of " + str(x) + " radians is " + str(round(cos(x), 3)))
-
-        elif choice == '12':
-            type_choice = rad_or_degree()
-            x = float(input("Number to be tanned: "))
-            
-            if type_choice == 0:
-                if cos(math.radians(x)) == 0:
-                    print("Tangent of ", x, " degrees is undefined")
-                    exit_flow("Tangent of " + str(x) + " degrees is undefined")
-                else:
-                    print("The tangent of ", x, " degrees is ", round(tan(math.radians(x)), 3))
-                    exit_flow("The tangent of " + str(x) + " degrees is " + str(round(tan(math.radians(x)), 3)))
-            elif type_choice == 1:
-                if cos(x) == 0:
-                    print("Tangent of ", x, " radians is undefined")
-                    exit_flow("Tangent of " + str(x) + " radians is undefined")
-                else:
-                    print("The tangent of ", x, " radians is ", round(tan(x), 3))
-                    exit_flow("The tangent of " + str(x) + " radians is " + str(round(tan(x), 3)))
-
-        elif choice == '13':
-            measure = float(input("Degree measure to be converted to radians: "))
-            print(measure, " degrees converted to radians is", rad((measure)), " radians ")
-            exit_flow(str(measure) + " degrees converted to radians is " + str(rad((measure))) + " radians ")
-
-        elif choice == '14':
-            measure = float(input("Radian measure to be converted to degrees: "))
-            print(measure, " radians converted to degrees is", deg(measure / 3.14 * math.pi), " degrees ")
-            exit_flow(str(measure) + " radians converted to degrees is " + str(deg(measure / 3.14 * math.pi)) + " degrees ")
-
-        elif choice == '15':
-            read_file()
-            exit_flow("15")        
-        
-        else: #  if operation choice is not within the range of 1-15, return error message to user and prompt them again
-            print("Invalid operation selected, please try again")
-            time.sleep(2)
-            calculator()
-
+#all helper functions
+def safeConvert(value : str, defaultValue : str, function):
+    try:
+        return function(value)
+    except ValueError:
+        return defaultValue
 
 def exit_flow(printed_message: str):
     
@@ -255,7 +44,7 @@ def write_file(what_to_write: str):
         print("Calculation saved to ", file_name)
 
 def read_file():
-    file_name = str(input("Which storage file would you like to read? ") + ".txt")
+    file_name = str(input("Which storage file would you like to read?") + ".txt")
     try:
         with open(file_name, mode='r') as file_object:
             # todo: implement ability for user to pick how many previous calculations to print, error handling for if user picks more lines than the file contains
@@ -269,5 +58,225 @@ def read_file():
 
     
 
+
+# adding
+
+def calculator():
+    def safeInputSingleValue(PromptMessage : str) -> float:
+        Value = None
+        while Value == None:
+            Value=safeConvert(input(PromptMessage), None, float)
+            
+        return Value
+    
+    def runAndRound2Value(x : float, y : float, func) -> float: 
+        return round(func(x, y),int(input("Rounding place: ")))
+    
+    def runAndRound1Value(x : float, func) -> float: 
+        return round(func(x),int(input("Rounding place: ")))
+        
+    def add(x : float, y : float) -> float:
+        return x + y
+
+    # subtracting
+
+    def subtract(x : float, y : float) -> float:
+        return x - y
+
+    # multiplying
+
+    def multiply(x : float, y : float) -> float:
+        return x * y
+
+    # dividing
+
+    def divide(x : float, y : float) -> float:
+        return x / y
+
+    # exponentiation
+
+    def exponent(x : float, y : float) -> float:
+        return x ** y
+
+    # modulo/remainder/whatever you want to call it
+
+    def modulo(x : float, y : float) -> float:
+        return x % y
+
+    # square root
+
+    def sqrroot(x : float) -> float:
+        return exponent(x, 2)
+
+    # circumference
+
+    def circumference(x : float) -> float:
+        return math.pi * x
+
+    # area of circle
+
+    def circle_area(x : float) -> float:
+        return math.pi * (x ** 2)
+
+    # sin, cos, tan
+
+    def sin(x : float) -> float:
+        return math.sin(x)
+
+    def cos(x : float) -> float:
+        return math.cos(x)
+
+    def tan(x : float) -> float:
+        return math.tan(x)
+
+    def rad(x : float) -> float:
+        return math.radians(x)
+
+    def deg(x : float) -> float:
+        return math.degrees(x)
+
+    def rad_or_degree() -> int: # rad or degree choice picker function
+        Vrad_or_degree = None
+        while Vrad_or_degree == None:
+            Vrad_or_degree = safeConvert(input("Degrees or radians? Enter 0 for degrees, 1 for radians: "), None, int)
+            if Vrad_or_degree == 1 or Vrad_or_degree == 0:
+                return Vrad_or_degree
+                break
+            else:
+                print("Must select 1 or 0")
+                time.sleep(2)
+
+    # User prompt to select operation
+
+    print("Select an operation")
+    print("1. add")
+    print("2. subtract")
+    print("3. multiply")
+    print("4. divide")
+    print("5. exponent")
+    print("6. modulo/remainder")
+    print("7. square root")
+    print("8. circumference")
+    print("9. area of circle")
+    print("10. sine")
+    print("11. cosine")
+    print("12. tangent")
+    print("13. convert degrees to radians")
+    print("14. convert radians to degrees")
+    print("15. read previous calculations")
+    print("-E to exit")
+
+    # Making the calculator work
+
+    while True:
+        # todo: fix spacing issues in output to file
+        # For every choice, execute operations based on user input
+
+        choice = input("Enter choice(1/2/3/4/5/6/7/8/9/10/11/12/13/14/15): ")
+        if choice.__eq__("-E"):
+            sys.exit(0)
+        
+        if choice in ('1', '2', '3', '4', '5', '6'):
+            number_1 = None
+            number_2 = None
+            
+            while number_1 == None or number_2 == None :
+                print("repeat if bad value is entered (bad values are anything that is not a number)")
+                number_1 = safeConvert(input("Enter first number: "), None, float)
+                number_2 = safeConvert(input("Enter second number: "), None, float)
+            
+            match choice:
+                case '1':
+                    print(number_1, "+", number_2, "=", runAndRound2Value(number_1, number_2, add))
+                    exit_flow(str(number_1) + "+" + str(number_2) + "=" + str(add(number_1, number_2)))
+                
+                case '2':
+                    print(number_1, "-", number_2, "=", runAndRound2Value(number_1, number_2, subtract))
+                    exit_flow(str(number_1) + "-" + str(number_2) + "=" + str(subtract(number_1, number_2)))
+
+                case '3':
+                    print(number_1, "*", number_2, "=", runAndRound2Value(number_1, number_2, multiply))
+                    exit_flow(str(number_1) + "*" + str(number_2) + "=" + str(multiply(number_1, number_2)))
+
+                case '4':
+                    if number_2 == 0: # if user attempts to input by 0, return error message
+                        print("Oops! Numbers cannot be divided by 0")
+                    else:
+                        print(number_1, "/", number_2, "=", runAndRound2Value(number_1, number_2, divide))
+                        exit_flow(str(number_1) + "/" + str(number_2) + "=" + str(divide(number_1, number_2)))
+    
+                case '5':
+                    print(number_1, "to the power of", number_2, "=", runAndRound2Value(number_1, number_2, exponent))
+                    exit_flow(str(number_1) + "to the power of" + str(number_2) + "=" + str(exponent(number_1, number_2)))
+                    
+                case '6':
+                    if number_2 == 0: # if user attempts to input by 0, return error message
+                        print("Oops! Numbers cannot be divided by 0")
+                    else:
+                        print(number_1, "/", number_2, "=", runAndRound2Value(number_1, number_2, modulo))
+                        exit_flow(str(number_1) + "/" + str(number_2) + "=" + str(modulo(number_1, number_2)))
+                        
+        else: 
+            number = safeInputSingleValue("Enter Number")
+            match choice:
+                case '7':
+                    print("The square root of", number, "is", runAndRound1Value(number, sqrroot))
+                    exit_flow("The square root of" + str(number) + "is" + str(sqrroot(number)))
+        
+                case '8':
+                    print("A circle with diameter", number, "has a circumference of", runAndRound1Value(number, circumference))
+                    exit_flow("A circle with diameter" + str(number) + "has a circumference of" + str(circumference(number)))
+        
+                case '9':
+                    print("A circle with radius", number, "has an area of", runAndRound1Value(number, circle_area))
+                    exit_flow("A circle with radius" + str(number) + "has an area of" + str(circle_area(number)))
+        
+                case '10':
+                    match rad_or_degree():
+                        case 0:
+                            print("The sine of ", number, " degrees is ", runAndRound1Value(math.radians(number), sin))
+                            exit_flow("The sine of " + str(number) + " degrees is " + str(round(sin(math.radians(number)), 3)))
+                        case 1:
+                            print("The sine of ", number, " radians is ", runAndRound1Value(number, sin))
+                            exit_flow("The sine of " + str(number) + " radians is " + str(round(sin(number), 3))) 
+        
+                case '11':
+                    match rad_or_degree:
+                        case 0:
+                            print("The cosine of ", number, " degrees is ", round(cos(math.radians(number)), 3))
+                            exit_flow("The cosine of " + str(number) + " degrees is " + str(round(cos(math.radians(number)), 3)))
+                        case 1:
+                            print("The cosine of ", number, " radians is ", round(cos(number), 3))
+                            exit_flow("The cosine of " + str(number) + " radians is " + str(round(cos(number), 3)))
+        
+                case '12':
+                    type_choice = rad_or_degree()
+                    if cos(math.radians(number) if type_choice==0 else number) == 0:
+                        print("Tangent of ", number, "degrees" if type_choice==0 else "radians", "is undefined")
+                        exit_flow("Tangent of " + str(number) + ("degrees" if type_choice==0 else "radians") + " is undefined")
+                    
+                    match type_choice:
+                        case 0:
+                                print("The tangent of ", number, " degrees is ", round(tan(math.radians(number)), 3))
+                                exit_flow("The tangent of " + str(number) + " degrees is " + str(round(tan(math.radians(number)), 3)))
+                        case 1:
+                                print("The tangent of ", number, " radians is ", round(tan(number), 3))
+                                exit_flow("The tangent of " + str(number) + " radians is " + str(round(tan(number), 3)))
+        
+                case '13':
+                    print(number, " degrees converted to radians is", rad((number)), " radians ")
+                    exit_flow(str(number) + " degrees converted to radians is " + str(rad((number))) + " radians ")
+        
+                case '14':
+                    print(number, " radians converted to degrees is", deg(number / 3.14 * math.pi), " degrees ")
+                    exit_flow(str(number) + " radians converted to degrees is " + str(deg(number / 3.14 * math.pi)) + " degrees ")
+                case '15':
+                    read_file()
+                    exit_flow("15")        
+                
+                case _: #  if operation choice is not within the range of 1-15, return error message to user and prompt them again
+                    print("Invalid operation selected, please try again")
+                    time.sleep(2)
+                
 calculator()
 # :)
