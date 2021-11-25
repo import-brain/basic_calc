d6a976e270f78e98cc7161363fab42ed75d7074e
Eric Cheng <ericcheng9316@gmail.com>
2021-11-25 00:15:26 -0500

    refactor: refactor current unit tests

--- calc_test.py
+++ calc_test.py
@@ -1,21 +1,20 @@
-import calc
+from calc import *
 
-class Test_Calc_Div_function:
-    def test_div_function_1(self):
-        result = calc.div_function([0, 0], ["1", "0.1"])
+if (add(1, 2) == 3 and add(2, 2) == 4):
+    print("Add test passed")
+else:
+    print("Add test failed: " + str(add(1, 2)))
 
-    def test_div_function_2(self):
-        result = calc.div_function([0, 1.0], ["1", "0.1"])
+if (subtract(2, 1) == 1 and subtract(2, 2) == 0):
+    print("Subtract test passed")
+else:
+    print("Subtract test failed: " + str(subtract(2, 1)))
 
-    def test_div_function_3(self):
-        result = calc.div_function([0, 0], ["1", "12.0"])
+if (multiply(2, 2) == 4 and multiply(2, 1) == 2):
+    print("Multiply test passed")
+else:
+    print("Multiply test failed: " + str(multiply(2, 2)))
 
-    def test_div_function_4(self):
-        result = calc.div_function([0, 0], ["10", "3.14"])
 
-    def test_div_function_5(self):
-        result = calc.div_function([0, -1.0], ["0.1", "3.14"])
 
-    def test_div_function_6(self):
-        result = calc.div_function([], [])
 
