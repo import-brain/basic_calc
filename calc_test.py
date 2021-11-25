dfcfc0d2bd21281cf353dcef92e08c04f24e990f
Eric Cheng <ericcheng9316@gmail.com>
2021-11-25 12:08:15 -0500

    tests: add more unit tests
    refactor unit tests to use functions, added comments

--- calc_test.py
+++ calc_test.py
@@ -1,20 +1,87 @@
-from calc import *
+"""
+unit tests for major functions in calc.py
+"""
+# import functions from calc.py
+from calc import add, subtract, multiply, divide
 
-if (add(1, 2) == 3 and add(2, 2) == 4):
-    print("Add test passed")
-else:
-    print("Add test failed: " + str(add(1, 2)))
+# title function for each suite of tests
+def test_title(test):
+    print(test + " tests")
+    print("--------------------")
 
-if (subtract(2, 1) == 1 and subtract(2, 2) == 0):
-    print("Subtract test passed")
-else:
-    print("Subtract test failed: " + str(subtract(2, 1)))
+def separator():
+    print("\n")
+    print("--------------------")
 
-if (multiply(2, 2) == 4 and multiply(2, 1) == 2):
-    print("Multiply test passed")
-else:
-    print("Multiply test failed: " + str(multiply(2, 2)))
+# adding tests
+def add_tests():
+    test_title("add")
 
+    try:
+        assert add(1, 2) == 3
+        assert add(1, 4) == 5
+        assert add(1, -4) == -3
+    except AssertionError:
+        print("Adding tests failed, check your code")
+        separator()
+        return 1
+    else:
+        print("Adding tests passed")
+        separator()
+        return 0
+# subtract tests
+def subtract_tests():
+    test_title("subtract")
 
+    try:
+        assert subtract(3, 7) == -4
+        assert subtract(1, 4) == -3
+        assert subtract(1, -4) == 5
+        assert subtract(0, 0) == 0
+    except AssertionError:
+        print("Subtracting tests failed, check your code")
+        separator()
+        return 1
+    else:
+        print("Subtracting tests passed")
+        separator()
+        return 0
+# multiply tests
+def multiply_tests():
+    test_title("multiply")
 
+    try:
+        assert multiply(2, 3) == 6
+        assert multiply(1, 4) == 4
+        assert multiply(1, -4) == -4
+        assert multiply(0, 5) == 0
+    except AssertionError:
+        print("Multiplying tests failed, check your code")
+        separator()
+        return 1
+    else:
+        print("Multiplying tests passed")
+        separator()
+        return 0
 
+def divide_tests():
+    test_title("divide")
+
+    try:
+        assert divide(6, 3) == 2
+        assert divide(4, 1) == 4
+        assert divide(1, -4) == -0.25
+        assert divide(0, 5) == 0
+    except AssertionError:
+        print("Dividing tests failed, check your code")
+        separator()
+        return 1
+    else:
+        print("Dividing tests passed")
+        separator()
+        return 0
+# call all test functions
+add_tests()
+subtract_tests()
+multiply_tests()
+divide_tests()
