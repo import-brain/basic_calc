8db0f01a29c487412abe1873034d49a4320f003c
Eric Cheng <ericcheng9316@gmail.com>
2021-11-25 13:03:42 -0500

    refactor: use pytest to test

--- calc_test.py
+++ calc_test.py
@@ -1,113 +1,33 @@
-"""
-unit tests for major functions in calc.py
-"""
-# import functions from calc.py
-from calc import (
-    add, 
-    subtract, 
-    multiply, 
-    divide, 
-    exponent,
-)
+# I know import * is bad convention, but I'm not sure how to do it better
+# without listing all the functions in calc.py and having a super long line
+from calc import *
+# test suite below
+def test_add():
+    assert add(1, 2) == 3
+    assert add(0, 0) == 0
+    assert add(-1, 1) == 0
+    assert add(1, -1) == 0
+    assert add(-1, -1) == -2
 
-# title function for each suite of tests
-def test_title(test):
-    print(test + " tests")
-    print("--------------------")
+def test_subtract():
+    assert subtract(5, 3) == 2
+    assert subtract(5, -3) == 8
+    assert subtract(-4, 3) == -7
+    assert subtract(-4, -3) == -1
+    assert subtract(0, 0) == 0
+    assert subtract(0, -1) == 1
 
-def separator():
-    print("\n")
-    print("--------------------")
+def test_multiply():
+    assert multiply(5, 3) == 15
+    assert multiply(5, -3) == -15
+    assert multiply(-4, 0) == 0
+    assert multiply(3, 0) == 0
+    assert multiply(0, 0) == 0
+    assert multiply(-1, -5) == 5
 
-# adding tests
-def add_tests():
-    test_title("add")
-
-    try:
-        assert add(1, 2) == 3
-        assert add(1, 4) == 5
-        assert add(1, -4) == -3
-    except AssertionError:
-        print("Adding tests failed, check your code")
-        separator()
-        return 1
-    else:
-        print("Adding tests passed")
-        separator()
-        return 0
-# subtract tests
-def subtract_tests():
-    test_title("subtract")
-
-    try:
-        assert subtract(3, 7) == -4
-        assert subtract(1, 4) == -3
-        assert subtract(1, -4) == 5
-        assert subtract(0, 0) == 0
-    except AssertionError:
-        print("Subtracting tests failed, check your code")
-        separator()
-        return 1
-    else:
-        print("Subtracting tests passed")
-        separator()
-        return 0
-# multiply tests
-def multiply_tests():
-    test_title("multiply")
-
-    try:
-        assert multiply(2, 3) == 6
-        assert multiply(1, 4) == 4
-        assert multiply(1, -4) == -4
-        assert multiply(0, 5) == 0
-    except AssertionError:
-        print("Multiplying tests failed, check your code")
-        separator()
-        return 1
-    else:
-        print("Multiplying tests passed")
-        separator()
-        return 0
-
-def divide_tests():
-    test_title("divide")
-
-    try:
-        assert divide(6, 3) == 2
-        assert divide(4, 1) == 4
-        assert divide(1, -4) == -0.25
-        assert divide(0, 5) == 0
-    except AssertionError:
-        print("Dividing tests failed, check your code")
-        separator()
-        return 1
-    else:
-        print("Dividing tests passed")
-        separator()
-        return 0
-
-def exponent_tests():
-    test_title("exponent")
-
-    try:
-        assert exponent(2, 3) == 8
-        assert exponent(1, 4) == 1
-        assert exponent(1, -4) == 1
-        assert exponent(0, 5) == 0
-        assert exponent(2, 0) == 1
-        assert exponent(9, 0.5) == 3.0
-    except AssertionError:
-        print("Exponent tests failed, check your code")
-        separator()
-        return 1
-    else:
-        print("Exponent tests passed")
-        separator()
-        return 0
-# call all test functions
-add_tests()
-subtract_tests()
-multiply_tests()
-divide_tests()
-exponent_tests()
+def test_divide():
+    assert divide(10, 2) == 5
+    assert divide(10, -2) == -5
+    assert divide(-10, 2) == -5
+    assert divide(-5, 5) == -1
+    assert divide(0, 1) == 0
